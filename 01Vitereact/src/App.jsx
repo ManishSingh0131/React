9e1f6b8d935c0120c280701d926239f528f706ea
Manish Kumar Singh <manishkumarsingh0131cs@gmail.com>
2025-03-08 03:58:56 +0530

    update my repo

--- 01Vitereact/src/App.jsx
+++ 01Vitereact/src/App.jsx
@@ -3,11 +3,12 @@ import Chai from "./Chai"
 import './App.css'
 
 function App() {
- 
+ const userName="Manish Singh"
 
   return (
     <>
       <Chai/>
+      <h3>Wlecome to react {userName}</h3>
     </>
   )
 }
--- 01Vitereact/src/main.jsx
+++ 01Vitereact/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+// const reactElement={
+//   type:"a",
+//   props:{
+//       href:"https://www.google.com",
+//       target:"_blank",
+//   },
+//   children:"Click me to visit google",
+
+// }
+const anotherElement=(
+  <a href="https://google.com" target="_blank"> Visit Google</a>
+);
+const reactElement =React.createElement(
+  "a",
+  {href:"https://google.com",target:"_blank"},
+  "click me to visit google"
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+    // <App />
+    // anotherElement
+    reactElement
+
 )
